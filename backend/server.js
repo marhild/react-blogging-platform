2ac41c0d66c0756677cf0a5841f911e8596dab36
Maria Hildebrandt <hildebrandt.maria@gmail.com>
2023-03-16 11:08:28 +0100

    routes for authentification

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 
 // routes
 const blogRoutes = require('./routes/blog')
+const authRoutes = require('./routes/auth')
 
 // app
 const app = express()
@@ -24,6 +25,7 @@ if(process.env.NODE_ENV == 'development' ){
 
 // routes middleware 
 app.use('/api', blogRoutes)
+app.use('/api', authRoutes)
 
 // port
 const port = process.env.REACT_APP_PORT || 8000
