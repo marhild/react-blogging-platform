e0f74db9db61d4c52a3918b49d3a160e93a9fbea
Maria Hildebrandt <hildebrandt.maria@gmail.com>
2023-03-21 14:20:47 +0100

    added signin, signout and secure route

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
 const shortId = require('shortid');
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
 
 exports.signup = (req, res) => {
     // console.log(req.body);
@@ -30,3 +32,42 @@ exports.signup = (req, res) => {
         });
     });
 };
+
+exports.signin = (req, res) => {
+    const { email, password } = req.body;
+    // check if user exist
+    User.findOne({ email }).exec((err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'User with that email does not exist. Please signup.'
+            });
+        }
+        // authenticate
+        if (!user.authenticate(password)) {
+            return res.status(400).json({
+                error: 'Email and password do not match.'
+            });
+        }
+        // generate a token and send to client
+        const token = jwt.sign({ _id: user._id }, process.env.REACT_APP_JWT_SECRET, { expiresIn: '1d' });
+
+        res.cookie('token', token, { expiresIn: '1d' });
+        const { _id, username, name, email, role } = user;
+        return res.json({
+            token,
+            user: { _id, username, name, email, role }
+        });
+    });
+};
+
+exports.signout = (req, res) => {
+    res.clearCookie("token")
+    res.json({
+        message: "Signout success"
+    })
+}
+
+//checks token expiration
+exports.requireSignin = expressJwt({
+    secret: process.env.REACT_APP_JWT_SECRET
+})
