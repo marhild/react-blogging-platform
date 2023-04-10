5a7dea0446db3d187e9391a505e12f5507e2443b
Maria Hildebrandt <hildebrandt.maria@gmail.com>
2023-04-10 17:14:17 +0200

    adds tags and categories to blogCreate

--- frontend/components/crud/BlogCreate.js
+++ frontend/components/crud/BlogCreate.js
@@ -24,6 +24,12 @@ const CreateBlog = ({router}) => {
         }
     }
 
+    const [categories, setCategories] = useState([])
+    const [tags, setTags] = useState([])
+
+    const [checked, setChecked] = useState([]); // categories
+    const [checkedTag, setCheckedTag] = useState([]); // tags
+
     const [body, setBody] = useState(blogFromLocalStorage())
     const [values, setValues] = useState({
         error: '',
@@ -35,10 +41,33 @@ const CreateBlog = ({router}) => {
     })
 
     const {error, sizeError, success, formData, title, hidePublishButon} = values
+    const token = getCookie('token');
 
     useEffect (() => {
         setValues({...values, formData: new FormData()})
+        initCategories()
+        initTags()
     }, [router])
+
+    const initCategories = () => {
+        getCategories().then(data => {
+            if(data.error) {
+                setValues({...values, error: data.error})
+            } else {
+                setCategories(data)
+            }
+        })
+    }
+
+    const initTags = () => {
+        getTags().then(data => {
+            if(data.error) {
+                setValues({...values, error: data.error})
+            } else {
+                setTags(data)
+            }
+        })
+    }
 
     const publishBlog = (e) => {
         e.preventDefault()
@@ -61,6 +90,72 @@ const CreateBlog = ({router}) => {
         }
     }
 
+    const handleToggle = c => () => {
+        setValues({ ...values, error: '' });
+        // return the first index or -1
+        const clickedCategory = checked.indexOf(c);
+        const all = [...checked];
+
+        if (clickedCategory === -1) {
+            all.push(c);
+        } else {
+            all.splice(clickedCategory, 1);
+        }
+        setChecked(all);
+        formData.set('categories', all);
+    };
+
+    const handleTagsToggle = t => () => {
+        setValues({ ...values, error: '' });
+        // return the first index or -1
+        const clickedTag = checked.indexOf(t);
+        const all = [...checkedTag];
+
+        if (clickedTag === -1) {
+            all.push(t);
+        } else {
+            all.splice(clickedTag, 1);
+        }
+        setCheckedTag(all);
+        formData.set('tags', all);
+    };
+
+    const showCategories = () => {
+        return (
+            categories &&
+            categories.map((c, i) => (
+                <li key={i} className="list-unstyled">
+                    <input onChange={handleToggle(c._id)} type="checkbox" className="mr-2" />
+                    <label className="form-check-label">{c.name}</label>
+                </li>
+            ))
+        );
+    };
+
+    const showTags = () => {
+        return (
+            tags &&
+            tags.map((t, i) => (
+                <li key={i} className="list-unstyled">
+                    <input onChange={handleTagsToggle(t._id)} type="checkbox" className="mr-2" />
+                    <label className="form-check-label">{t.name}</label>
+                </li>
+            ))
+        );
+    };
+
+    const showError = () => (
+        <div className="alert alert-danger" style={{ display: error ? '' : 'none' }}>
+            {error}
+        </div>
+    );
+
+    const showSuccess = () => (
+        <div className="alert alert-success" style={{ display: success ? '' : 'none' }}>
+            {success}
+        </div>
+    );
+
     const createBlogForm = () => {
         return(
             <form onSubmit={publishBlog}>
@@ -80,7 +175,46 @@ const CreateBlog = ({router}) => {
         )
     }
 
-    return (<div>{createBlogForm()}</div>)
+    return (
+        <div className="container-fluid pb-5">
+            <div className="row">
+                <div className="col-md-8">
+                    {createBlogForm()}
+                    <div className="pt-3">
+                        {showError()}
+                        {showSuccess()}
+                    </div>
+                </div>
+
+                <div className="col-md-4">
+                    <div>
+                        <div className="form-group pb-2">
+                            <h5>Featured image</h5>
+                            <hr />
+
+                            <small className="text-muted">Max size: 1mb</small>
+                            <br />
+                            <label className="btn btn-outline-info">
+                                Upload featured image
+                                <input onChange={handleChange('photo')} type="file" accept="image/*" hidden />
+                            </label>
+                        </div>
+                    </div>
+                    <div>
+                        <h5>Categories</h5>
+                        <hr />
+
+                        <ul style={{ maxHeight: '200px', overflowY: 'scroll' }}>{showCategories()}</ul>
+                    </div>
+                    <div>
+                        <h5>Tags</h5>
+                        <hr />
+                        <ul style={{ maxHeight: '200px', overflowY: 'scroll' }}>{showTags()}</ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 
 }
 
